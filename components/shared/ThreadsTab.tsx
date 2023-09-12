adcf48b66ab82ee4ecfbe30e7047e17fedcc4a7b
Muhammad Samir <79057248+MuhammadSamir1212@users.noreply.github.com>
2023-09-12 18:51:12 +0300

    Update ThreadsTab.tsx

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,7 +1,36 @@
+import { redirect } from "next/navigation";
+
+import { fetchCommunityPosts } from "@/lib/actions/community.actions";
 import { fetchUserPosts } from "@/lib/actions/user.actions";
 
-import { redirect } from "next/navigation";
 import ThreadCard from "../cards/ThreadCard";
+
+interface Result {
+  name: string;
+  image: string;
+  id: string;
+  threads: {
+    _id: string;
+    text: string;
+    parentId: string | null;
+    author: {
+      name: string;
+      image: string;
+      id: string;
+    };
+    community: {
+      id: string;
+      name: string;
+      image: string;
+    } | null;
+    createdAt: string;
+    children: {
+      author: {
+        image: string;
+      };
+    }[];
+  }[];
+}
 
 interface Props {
   currentUserId: string;
@@ -9,18 +38,22 @@ interface Props {
   accountType: string;
 }
 
-export default async function ThreadsTab({
-  currentUserId,
-  accountId,
-  accountType,
-}: Props) {
-  let result = await fetchUserPosts(accountId);
+async function ThreadsTab({ currentUserId, accountId, accountType }: Props) {
+  let result: Result;
 
-  if (!result) redirect("/");
+  if (accountType === "Community") {
+    result = await fetchCommunityPosts(accountId);
+  } else {
+    result = await fetchUserPosts(accountId);
+  }
+
+  if (!result) {
+    redirect("/");
+  }
 
   return (
-    <section className="mt-9 flex flex-col gap-10">
-      {result.threads.map((thread: any) => (
+    <section className='mt-9 flex flex-col gap-10'>
+      {result.threads.map((thread) => (
         <ThreadCard
           key={thread._id}
           id={thread._id}
@@ -48,3 +81,5 @@ export default async function ThreadsTab({
     </section>
   );
 }
+
+export default ThreadsTab;
